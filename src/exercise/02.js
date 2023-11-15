28a8840e5faf473fd698678afe8fb337b5db8fd5
slolow <laszlo.starost@barmer.de>
2023-11-15 16:11:18 +0100

    refactor: :technologist: extra-credit-4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,12 +3,24 @@
 
 import * as React from 'react'
 
-const useLocalStorageState = (key, defaultValue = '') => {
-  const [state, setState] = React.useState(() => window.localStorage.getItem(key) ?? defaultValue) 
+const useLocalStorageState = (key, defaultValue = '', {serialize = JSON.stringify, deserialize = JSON.parse} = {}) => {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  }) 
+  const prevKeyRef = React.useRef(key);
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, state)
-  }, [key, state])
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [serialize, key, state])
 
   return [state, setState]
 }
